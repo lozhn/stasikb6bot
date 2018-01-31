1aaaf8a5ec21644203c4e79c8899e7c66c030638
KatrinYudina <yudina.k8@gmail.com>
2018-01-31 05:04:16 +0100

    Added description to annotated code

--- bot/calculator.py
+++ bot/calculator.py
@@ -6,6 +6,8 @@ def calculator(env):
     def _calculator(bot, update):
         res = client.query(update.message.text)
         bot.send_message(chat_id=update.message.chat_id, text=next(res.results).text)
+		
+		# Using next loop, bot scratches all variants of solution, even writes out in words
         # for pod in res.pod:
         #     for sub in pod.subpod:
         #         answer = "{} : {}".format(sub["img"]["@src"], sub["img"]["@alt"])
