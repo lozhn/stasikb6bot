4014acb1c11e9e38be4134d056b4c1b72304180b
Mikhail Kizilov <kizilov.mikhail@gmail.com>
2018-01-31 23:21:27 +0300

    xo 5 in row

--- bot/bot.py
+++ bot/bot.py
@@ -9,12 +9,21 @@ import wolframalpha
 
 logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+<<<<<<< HEAD
 def common_cb_handler(bot, update):
     path, val = update.callback_query.data.split('_')
     update.callback_query.data = val
 
     if path == 'tictactoe':
         tictactoe_cb(bot, update)
+=======
+from bot.tictactoe import tictactoe, button
+from bot.calculator import calculator
+from bot.xo import xo_game, xo_move, XOFilter
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+>>>>>>> xo 5 in row
 
     if path == 'matches':
         matches_cb(bot, update)
@@ -29,6 +38,7 @@ def run(env):
 
     updater = Updater(env['TELEGRAM_TOKEN'])
 
+<<<<<<< HEAD
     help = help_cmd_factory(env['HELP_TEXT'])
     start = start_cmd
     client = wolframalpha.Client(env['WOLFRAM_TOKEN'])
@@ -46,6 +56,17 @@ def run(env):
 
     updater.dispatcher.add_handler(MessageHandler(Filters.text, _calculator_cmd))
     updater.dispatcher.add_handler(CallbackQueryHandler(common_cb_handler))
+=======
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    updater.dispatcher.add_handler(CommandHandler('help',  help))
+    updater.dispatcher.add_handler(CommandHandler('tictactoe', tictactoe))
+    updater.dispatcher.add_handler(CommandHandler('xo',  xo_game))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    #updater.dispatcher.add_handler(MessageHandler(Filters.text, calculator))
+
+    updater.dispatcher.add_handler(MessageHandler(XOFilter(), xo_move))
+>>>>>>> xo 5 in row
 
     updater.start_polling()
     updater.idle()
